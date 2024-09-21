81cac1f493d5cced69c498af074c33a0aea07723
Saulo Barros <saulobarros1997@gmail.com>
2024-09-21 12:38:51 +0100

    finishing the order test

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -26,4 +26,38 @@ test('@Web Client App login', async ({ page }) => {
    await page.locator("div li").first().waitFor();
    const itemIsVisible = await page.locator("h3:has-text('zara coat 3')").isVisible();
    expect(itemIsVisible).toBeTruthy();
+
+   await page.locator("text=Checkout").click();
+
+   await page.locator("[placeholder*='Country']").pressSequentially("bra");
+   const dropdown = page.locator(".ta-results");
+   await dropdown.waitFor();
+   const optionsCount = await dropdown.locator("button").count();
+   for (let i = 0; i < optionsCount; ++i) {
+      const text = await dropdown.locator("button").nth(i).textContent();
+      if (text === " Brazil") {
+         await dropdown.locator("button").nth(i).click();
+         break;
+      }
+   }
+
+   expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+   await page.locator(".action__submit").click();
+   await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+   const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+
+   await page.locator("button[routerlink*='myorders']").click();
+   await page.locator("tbody").waitFor();
+   const rows = await page.locator("tbody tr");
+
+
+   for (let i = 0; i < await rows.count(); ++i) {
+      const rowOrderId = await rows.nth(i).locator("th").textContent();
+      if (orderId.includes(rowOrderId)) {
+         await rows.nth(i).locator("button").first().click();
+         break;
+      }
+   }
+   const orderIdDetails = await page.locator(".col-text").textContent();
+   expect(orderId.includes(orderIdDetails)).toBeTruthy();
 });
